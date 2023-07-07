4a2a89a563526828c6c34037fdd0555c154d2436
SagarSuryawan <sagarsurya45@gmail.com>
2023-07-07 14:51:02 +0530

    problem 5 and 6

---
+++ HOF/problem_5/index.js
@@ -0,0 +1,5 @@
+
+
+// Write a program using a regex that matches valid URLs. Valid URLs should start with either http:// or https://,followed by one or more letters, digits, or special characters, followed by a dot, followed by one or more letters.Print a message indicating if the input matches the conditions or not.
+
+
