bb9cfcc71445e9ea24ad9386ed70e483e3477bf9
SagarSuryawan <sagarsurya45@gmail.com>
2023-07-21 19:29:24 +0530

    try and catch understood and also taken notes

--- error handling_prototypes/try&catch/index.js
+++ error handling_prototypes/try&catch/index.js
@@ -1,2 +1,25 @@
 // try  & catch  
 
+// try catch is a error handing or expection .It used to find and tackel to errors.
+
+// syntax  
+
+try{
+    // is is a block of code that will check if there any errors in this block of code.If error found it will ignore try block and immediatly execute the catch block .
+
+    let x = undefined
+
+    console.log(x[0])
+
+}catch(err){
+
+    // it is used to address the error.catch block also have agrument "err",it used to print error ,find error which types of error it is
+    console.log(err," sagar,error found")
+}
+
+
+// whith the help of try and catch if error found it shows that error detail and execute the furter code.
+
+
+console.log("end")
+console.log(56)
