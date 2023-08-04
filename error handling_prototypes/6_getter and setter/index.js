3a691d4ae3f0c5b2b7b6df2024661b7b9aee2fe8
SagarSuryawan <sagarsurya45@gmail.com>
2023-08-04 18:53:27 +0530

    getter and setter are incomplete

---
+++ error handling_prototypes/6_getter and setter/index.js
@@ -0,0 +1,14 @@
+// getter and setter
+
+class friends{
+
+    constructor(o,t){
+
+        this.one = o;
+        this.two = t
+    }
+}
+
+
+let group = new friends("sagar","nikhil")
+console.log(group)
