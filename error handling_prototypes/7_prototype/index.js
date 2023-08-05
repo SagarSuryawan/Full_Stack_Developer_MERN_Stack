d35166d8c7a5c9501be999a528bf3589a84f28c6
SagarSuryawan <sagarsurya45@gmail.com>
2023-08-05 18:09:55 +0530

    understood  prototype

---
+++ error handling_prototypes/7_prototype/index.js
@@ -0,0 +1,34 @@
+// explaining prototype
+
+// in javascript every thing is object beacause object protpertise is attached.
+// to  add property from one to another 
+
+let obj1 = {
+    name:"sagar",
+    add:"bhandara"
+}
+let obj = {
+    phone:9096519472,
+    last_name: "Suryawanshi"
+}
+Object.setPrototypeOf(obj1,obj)
+
+
+
+
+
+// creating custom prototoype
+function Person(name) {
+    this.name = name;
+  }
+
+
+  Person.prototype.sayHello = function() {
+    console.log(`Hello, my name is ${this.name}`);
+  };
+  
+
+  const p = new Person('sagar');
+  console.log(p)
+  p.sayHello();
+  
