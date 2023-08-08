94c368c82718aec379942908c96aadcb42a1568d
SagarSuryawan <sagarsurya45@gmail.com>
2023-08-08 19:15:55 +0530

    closer

--- error handling_prototypes/8_clouser/index.js
+++ error handling_prototypes/8_clouser/index.js
@@ -1,4 +1,8 @@
 // clouser
+
+// clouser are the function  that "remember" the variables from the outer enviorment where they were created,even if they used in different contex.
+
+// EXAMPLE:-
 function outer(){
 
     const outer = 2;
@@ -14,5 +18,26 @@ function outer(){
 
 outer();
 
+// LEXIAL SCOPING
 
 // lexial scoping is inner function hava access of outer vairable and outer function does not vhave access of inner variable.
+// EXAMPLE:-
+
+function outerState(){
+
+    const item = "Dell laptop";
+
+    function innerstate(){
+
+        console.log(item)
+    }
+
+    innerstate();
+}
+
+outerState();
+
+
+// outerstate() create a local variable "item" and a function called "innerstate".The innerstate() function is an inner function that defined inside outerState() and is available only within the body of the outerState function.inner function has no local variable or its own.since inner function  have access to the variable of outer funtion.
+
+
