0df562f8a844c7314f3e371b16f0a9631a57fd4e
SagarSuryawan <sagarsurya45@gmail.com>
2023-08-19 21:36:51 +0530

    problem six is solved

---
+++ Callback &Promises_L5/assignment/problem6.js
@@ -0,0 +1,11 @@
+// Write a function that asynchronously fetches data from an API
+// [ https://jsonplaceholder.typicode.com/todos/1 ]and logs the result to the console.
+
+async function fetchData(){
+    const response = await
+    fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await response.json();
+    console.log(data)
+}
+
+fetchData()
