6062d9617735bf8d2989da268b9b6ac182b38a4b
SagarSuryawan <sagarsurya45@gmail.com>
2023-08-19 18:32:41 +0530

    problem two is sloved

---
+++ Callback &Promises_L5/assignment/problem2.js
@@ -0,0 +1,18 @@
+// Write a function “manipulateString” that takes in a string and converts the characters to uppercase letters. The
+// Write a function "manipulateString" that takes in a string and converts the characters to uppercase letters. The
+// function should return a callback function "logString" that logs the sentence "The manipulated string is: " along
+// with the manipulated string or the new string to the console.
+
+
+function manipulateString (inputString,callback){
+    const manipulated = inputString.toUpperCase();
+
+    callback(manipulated);
+}
+
+
+function longString(manipulated) {
+    console.log(`Upercase string = ${manipulated}`)
+}
+
+manipulateString("pw skills",longString)
