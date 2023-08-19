49b24132acf0db1042b73d5cd9261f0bc1c0edfa
SagarSuryawan <sagarsurya45@gmail.com>
2023-08-19 19:27:25 +0530

    problem three is sloved

---
+++ Callback &Promises_L5/assignment/problem3.js
@@ -0,0 +1,31 @@
+// Write a function that takes in an array of integers and a callback function, and returns a new array where each
+// Write a JavaScript function called ageInDays that accepts an object containing a person's first name, last
+// name, and age in years as input. The function should concatenate the first and last name into a single string
+// and store it in a variable called fullName. It should then calculate the person's age in days and store it in a
+// variable called ageInDays.
+// The ageInDays function should then return a callback function that logs a message to the console. The
+// message should include the person's full name and age in days, and should be in the format: "The person's full
+// name is [full name] and their age in days is [age in days]."
+// Note that the ageInDays function should not log the message to the console directly, but should instead return
+// a callback function that can be used to log the message at a later time.
+
+
+
+const person = {
+    name:"sagar",
+    last_name:"Suryawanshi",
+    age:29
+}
+
+function ageInDays(personObject,callback){
+    const fullName = `${personObject.name} ${personObject.last_name}`
+    const ageInDays = personObject.age * 365
+    
+    callback(fullName,ageInDays)
+}
+
+function result(fullName,ageInDays){
+    console.log(`The person full name is ${fullName} and their age in day is ${ageInDays}`)
+}
+
+ageInDays(person,result)
