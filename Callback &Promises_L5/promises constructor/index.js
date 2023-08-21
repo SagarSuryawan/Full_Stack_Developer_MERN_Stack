462f8c22c1e0e709b1f6856c1917c4dba5c303bc
SagarSuryawan <sagarsurya45@gmail.com>
2023-08-21 14:55:26 +0530

    promises

--- Callback &Promises_L5/promises constructor/index.js
+++ Callback &Promises_L5/promises constructor/index.js
@@ -1,1 +1,33 @@
- 
+ const mypromise = new Promise((resolve,reject) => {
+
+let randomNum = Math.random()
+
+if(randomNum > 0.5){
+    resolve(`The promise is resolved,Greather than ${0.5}`)
+}else{
+    reject(`The promise is Rejected ,Less than ${0.5}`)
+}  
+
+ })
+
+ console.log(mypromise)
+
+
+
+//  .then and .catch
+
+
+// mypromise.then((result) =>{
+//     console.log(mypromise)
+// }).catch((err)
+//     console.log("err",err)
+// )
+   
+
+mypromise.then((result) => {
+    console.log(result)
+})
+
+.catch((error) => {
+    console.log(error)
+});
