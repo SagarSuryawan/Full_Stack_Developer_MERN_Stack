39463102483f1cd5fcc4d93e9704e1c1ac278299
SagarSuryawan <sagarsurya45@gmail.com>
2023-08-23 15:00:28 +0530

    working of function is done

---
+++ working_of_javascript_L6/working_function.js
@@ -0,0 +1,17 @@
+// Working of function in javascript
+
+// when javascript code begain to execute their is global execution contex is created it is enviorment where javascript code runs .
+ 
+// global execution contex has two code and memory.
+
+// code parts scan a whole code and all variables assign to undefined.
+
+// After scaning a code memory part assign values to given variables and code runs line by another.
+
+// If any funcction gets called, In call Stack first golbal execution contex is formed and then function execution contex is created ,Onces function completes it's execution it will remove from call stack.
+
+// Call Stack moniter the function  execution and working if function.
+
+// Call Stack works LIFO (last in first out).
+
+
