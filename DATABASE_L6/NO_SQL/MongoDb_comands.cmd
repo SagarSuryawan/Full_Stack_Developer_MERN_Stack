acfbd34cdef9bdf08077a77f432e4b986ca2f9fa
SagarSuryawan <sagarsurya45@gmail.com>
2023-09-20 13:47:17 +0530

    nosql mongodb commands

---
+++ DATABASE_L6/NO_SQL/MongoDb_comands.cmd
@@ -0,0 +1,20 @@
+1) open MongoDB in terminal.
+==> mongosh        [it will open the mongodb console]
+
+
+2) list all databases in MongoDB.
+==> show databases;  OR  show dbs;      [it will list all database]
+
+3)Select particular database.
+==> use name_of_database;        [it will select database to work on it]
+
+4)How to print all collection stored in a database.
+==> show collections;
+
+5)Print all the documents of a collection.
+==> db.collection_name.find();    [it will print all documents of collection]
+
+6)Create a new database.        
+==>use new_database_name;       [use keyword creats a new database if there is no already present db with same name] 
+
+7) If new database is empty then it will not list new database .Beacause in MongoDb if database is empty it will not show in list of databases;
