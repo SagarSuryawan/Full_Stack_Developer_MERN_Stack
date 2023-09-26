b2cd39b8bb126b2c2f2970ba60296ddb8b6d4fa0
SagarSuryawan <sagarsurya45@gmail.com>
2023-09-26 17:34:27 +0530

    notes

---
+++ ALL Notes/JAVASCRIPT/Array,Destructor/setandMap.js
@@ -0,0 +1,25 @@
+let emptySet = new Set()
+
+// console.log(emptySet.size);
+
+let myArray = [1, 2, 3, 4, 3, 2]
+let newSet = new Set([...myArray])
+console.log(newSet);
+
+newSet.add(9)
+newSet.clear()
+console.log(newSet);
+// console.log(newSet.has(9));
+
+// Set difference
+
+function setDifference(setA, setB){
+
+    return new Set([...setA].filter(el => !setB.has(el)))
+}
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let map = new Map()
+
+console.log(map.size);
