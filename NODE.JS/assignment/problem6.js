0d3b9d0f73e4c336ce88ca1e6eecc85513b94177
SagarSuryawan <sagarsurya45@gmail.com>
2023-10-06 17:11:09 +0530

    problem6

---
+++ NODE.JS/assignment/problem6.js
@@ -0,0 +1,9 @@
+
+
+const os = require('os');
+
+const osName = os.platform();
+console.log('Operating System:', osName);
+
+const osRelease = os.release();
+console.log('Release Version:', osRelease);
