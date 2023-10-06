b79bda298b742685f8ca69063a7a6f31045b4c9d
SagarSuryawan <sagarsurya45@gmail.com>
2023-10-06 16:32:01 +0530

    problem 3

--- NODE.JS/assignment/problem3/index.js
+++ NODE.JS/assignment/problem3/index.js
@@ -1,13 +1,12 @@
-// index.js
+const fs = require("fs");
 
-const fs = require('fs');
 
-fs.readFile('nodejs_architecture.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    return err;
-  }else{
-    console.log("Data:",data);
-  }
-  
-});
+const data = "Node.js is a platfrom that utlilize javascript and is primarily empolyed for developing web applicationthat are highly focused on input/outputoprations,but not limited to chat apllication and multimediawebsites.The Platfrom is constructedusing google chrome's V8 javascript engine.A web application is a type of software that executes on a server and is displayed by a client's browser that obtain all the application's resources over the internet ";
+
+fs.writeFile('nodejs_architecture.txt', data, (err) => {
+    if(err){
+        console.log("Error",err);
+    }else{
+        console.log(data)
+    }
+  });
