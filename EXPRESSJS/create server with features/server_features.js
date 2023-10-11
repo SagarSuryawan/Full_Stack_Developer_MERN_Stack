4af9e03b19e25ff9bb986597ce70b7f8c1a04bd2
SagarSuryawan <sagarsurya45@gmail.com>
2023-10-11 17:07:10 +0530

    server to server request

--- EXPRESSJS/create server with features/server_features.js
+++ EXPRESSJS/create server with features/server_features.js
@@ -1,5 +1,7 @@
 // features of server
 
+//  Sending HTTP request - The http.request() method can be used to send HTTP requests from a Node.js
+
 
 // http module is buit in module in Node.js providing functionality for creating and interating with http server and client.
 // it provides simple way to create http server and handel http responses.
@@ -8,14 +10,32 @@
 const http = require("http")
 // http.request()  is a function in node.js that is a part in http module,it isd used to make http request to other servers.
 
+
 const option = {
     hostname:"fakestoreapi.com",
-    path:"/products/1",
+    path:"/products/3",
     method:"GET"
 }
 
-const request = http.request(option,(res){
-    res.on("data",(d) =>{
-        process.stdout.write(d)
+
+// const apiReq = http.request(option,(apiRes)=>{
+//     apiRes.on("data",(data) =>{
+//         console.log(data.toString())
+//     })
+
+//     apiReq.on("error",()=>{
+//         console.log(e)
+//     })
+// })
+// apiReq.end()
+
+const apiReq = http.request(option,(apiRes) => {
+    apiRes.on("data",(data) =>{
+        console.log(data.tostring())
+    }) 
+
+    apiReq.on("error",() =>{
+        console.log(e)
     })
 })
+apiReq();
