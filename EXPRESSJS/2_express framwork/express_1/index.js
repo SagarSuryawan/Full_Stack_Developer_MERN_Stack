69209b157efea37a9fc6405c1074d610f49e9024
SagarSuryawan <sagarsurya45@gmail.com>
2023-10-14 20:30:55 +0530

    create expreejs enviorment,server,multiple path

---
+++ EXPRESSJS/2_express framwork/express_1/index.js
@@ -0,0 +1,34 @@
+// const express = require("express");
+// const app = express();
+// const port = 4999;
+
+// app.get('/',(req,res) => {
+//     res.send('Hello world')
+// })
+
+// app.listen(port,() =>{
+//     console.log(`server running at ${port}`)
+// })
+
+
+// =============================================================================
+
+
+
+const express = require("express");
+const app = express();
+const port = 4999;
+
+app.get('/',(req,res) => {
+    res.send('Hello world')
+})
+app.get('/about',(req,res) => {
+    res.send('about page')
+})
+app.get('/contact',(req,res) => {
+    res.send('contact page')
+})
+
+app.listen(port,() =>{
+    console.log(`server running at ${port}`)
+})
