5d9656896a8d06f0360bdf26ca1b5f47437c7d9d
SagarSuryawan <sagarsurya45@gmail.com>
2023-10-15 16:28:19 +0530

    install express and create server

--- EXPRESSJS/2_express framwork/express_1/index.js
+++ EXPRESSJS/2_express framwork/express_1/index.js
@@ -15,20 +15,42 @@
 
 
 
-const express = require("express");
-const app = express();
-const port = 4999;
+// const express = require("express");
+// const app = express();
+// const port = 4999;
 
-app.get('/',(req,res) => {
-    res.send('Hello world')
+// app.get('/',(req,res) => {
+//     res.send('Hello world')
+// })
+// app.get('/about',(req,res) => {
+//     res.send('about page')
+// })
+// app.get('/contact',(req,res) => {
+//     res.send('contact page')
+// })
+
+// app.listen(port,() =>{
+//     console.log(`server running at ${port}`)
+// })
+
+
+
+const express = require("express")
+const app = express();
+const port = 7001;
+
+
+app.get('/',(req,res) =>{
+    res.send("HOme Page")
 })
 app.get('/about',(req,res) => {
-    res.send('about page')
-})
+        res.send('about page')
+    })
 app.get('/contact',(req,res) => {
-    res.send('contact page')
-})
+        res.send('contact page')
+    })
 
-app.listen(port,() =>{
-    console.log(`server running at ${port}`)
-})
+
+    app.listen(port,() =>{
+        console.log(`server running on ${port} port`)
+    })
