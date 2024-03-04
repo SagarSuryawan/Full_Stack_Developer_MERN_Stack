30b9bac0f246876e502c924434c5034f683a0ec5
SagarSuryawan <sagarsurya45@gmail.com>
2024-03-04 20:21:19 +0530

    get user method id complete

--- Project/backend/user_info_logout_3/middleware/jwtAuth.js
+++ Project/backend/user_info_logout_3/middleware/jwtAuth.js
@@ -1,8 +1,11 @@
+//  function called jwtAuth that is designed to verify the authenticity of a JWT (JSON Web Token) provided by a client. It is typically used to protect routes that require a valid authentication token to access.
+
 const JWT = require("jsonwebtoken")
 
 const jwtAuth = (req,res,next) => {
 
 const token = (req.cookies && req.cookies.token) || null
+//req.cookies object exists and contains a property named token. If it does, it assigns the value of req.cookies.token to the token variable. If req.cookies is undefined or does not contain a property named token, the token variable is assigned the value null.
 
 if(!token){
     return res.status(400).json({
